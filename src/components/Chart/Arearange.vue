4d9ca4c6e96ce8bd1e501d8ae4995600fa2e46f8
Cheol_Kim <cheol.kim@fitogether.com>
2020-07-15 17:48:55 +0900

    Save Temp

---
+++ src/components/Chart/Arearange.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <highcharts class="hc" :options="chartOptions" ref="chart"></highcharts>
+  </div>
+</template>
+
+<script>
+// https://www.highcharts.com/demo/arearange-line
+export default {
+  data() {
+    return {
+      chartOptions: {
+        series: [
+          {
+            data: [1, 2, 3]
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
